64b01ed0656196cf468b2e1a106b352a0d19b4d9
Fousq <fous363@gmail.com>
2018-05-11 23:45:36 +0600

    Project files

---
+++ Demograph.cpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+USEFORM("MainUnit.cpp", MainForm);
+USEFORM("DataModule.cpp", DataModule1); /* TDataModule: File Type */
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+        try
+        {
+                 Application->Initialize();
+                 Application->CreateForm(__classid(TMainForm), &MainForm);
+                 Application->CreateForm(__classid(TDataModule1), &DataModule1);
+                 Application->Run();
+        }
+        catch (Exception &exception)
+        {
+                 Application->ShowException(&exception);
+        }
+        catch (...)
+        {
+                 try
+                 {
+                         throw Exception("");
+                 }
+                 catch (Exception &exception)
+                 {
+                         Application->ShowException(&exception);
+                 }
+        }
+        return 0;
+}
+//---------------------------------------------------------------------------
